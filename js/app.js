0c577c1f30e02c5b6a920c8da26293e892688276
Maxime <plaquin.maxime@gmail.com>
2016-01-20 01:32:54 +0100

    AngularFire
    Integration d’une authentification avec angularFire

--- js/app.js
+++ js/app.js
@@ -1,4 +1,34 @@
-var app = angular.module("app", []);
+app = angular.module('app', ['firebase']);
 
-var ref = new firebase ("https://brilliant-fire-1044.firebaseio.com/article");
-ref.push ({title: "Ouverture du bar", description:"ouverture du bar pour croque monsieur"});
+app.controller('AuthCtrl', [
+  '$scope', '$rootScope', '$firebaseAuth', function($scope, $rootScope, $firebaseAuth) {
+    var ref = new Firebase('https://brilliant-fire-1044.firebaseio.com');
+    $rootScope.auth = $firebaseAuth(ref);
+
+    $scope.signIn = function () {
+      $rootScope.auth.$login('password', {
+        email: $scope.email,
+        password: $scope.password
+      }).then(function(user) {
+        $rootScope.alert.message = '';
+      }, function(error) {
+        if (error = 'INVALID_EMAIL') {
+          console.log('email invalid or not signed up — trying to sign you up!');
+          $scope.signUp();
+        } else if (error = 'INVALID_PASSWORD') {
+          console.log('wrong password!');
+        } else {
+          console.log(error);
+        }
+      });
+    }
+
+
+  }
+]);
+
+app.controller('AlertCtrl', [
+  '$scope', '$rootScope', function($scope, $rootScope) {
+    $rootScope.alert = {};
+  }
+]);
